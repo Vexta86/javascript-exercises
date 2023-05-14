d8d1e8d66e2d72a1e12746f5aa2a4e8c42048c3d
Vexta86 <sebasrodsua05@gmail.com>
2023-05-14 10:53:22 -0500

    I finished up to the 7th exercise, I will try to do today the calculator

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if (isNaN(a) || isNaN(b) || a < 0 || b < 0 || (typeof a == "string") || (typeof b == "string")){
+        return("ERROR")
+    }
+    else{
+        let sum = 0;
+        if (a>b){
+            let g = a;
+            a = b;
+            b = g;
+        }
+        for (let i = a; i <= b; i ++){
+            sum += i
+        }
+        return(sum)
+    }
+
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,28 @@
-const leapYears = function() {
+const divisble = function(n, by){
+    if (n % by === 0){
+        return true
+    }
+    else{ 
+        return false 
+    }
+}
 
+const leapYears = function(year) {
+    if (divisble(year,4)){
+        if (divisble(year, 100)){
+            if(divisble(year,400)){
+                return true
+            }
+            else{
+                return false
+            }
+
+        }
+        return true
+    }
+    else{
+        return false
+    }
 };
 
 // Do not edit below this line
